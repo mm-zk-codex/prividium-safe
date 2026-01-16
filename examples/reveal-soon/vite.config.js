c5321d10029df84ae2cd7510f107e292816574b8
mm-zk <mm@matterlabs.dev>
2026-01-16 22:25:46 +0100

    fixed callback issue

--- examples/reveal-soon/vite.config.js
+++ examples/reveal-soon/vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        callback: resolve(__dirname, 'auth/callback.html'),
+      },
+    },
+  },
+
 });
